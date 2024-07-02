48ee1c3b33705756fef58924e3bf217a098bddc0
tamir avraham <tamir.avrah@gmail.com>
2024-07-02 16:05:43 +0300

    added filters for all of the different classes

--- Magshi Trivia For Web Back End/TcpServer.cpp
+++ Magshi Trivia For Web Back End/TcpServer.cpp
@@ -21,37 +21,6 @@ tcp::TcpServer::TcpServer(int port, std::string ip):_ip_address(ip),_port(port),
         logger.log(ss.str());
     }
     startListen();
-
-   /* int iResult = WSAStartup(MAKEWORD(2, 2), &_wsaData);
-    if (iResult != 0) {
-        printf("WSAStartup failed: %d\n", iResult);
-        throw INIT_ERROR;
-    }
-    struct addrinfo* result = NULL, * ptr = NULL, hints;
-
-    ZeroMemory(&hints, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_protocol = IPPROTO_TCP;
-    hints.ai_flags = AI_PASSIVE;
-
-
-    iResult = getaddrinfo(NULL, port.c_str(), &hints, &result);
-    if (iResult != 0) {
-        printf("getaddrinfo failed: %d\n", iResult);
-        WSACleanup();
-        throw INIT_ERROR;
-    }
-
-    _ListenSocket = INVALID_SOCKET;
-    _ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-
-    if (_ListenSocket == INVALID_SOCKET) {
-        printf("Error at socket(): %ld\n", WSAGetLastError());
-        freeaddrinfo(result);
-        WSACleanup();
-        throw INIT_ERROR;
-    }*/
 }
 
 tcp::TcpServer::~TcpServer()
